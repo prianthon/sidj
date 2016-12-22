0d943610de90fb3e802be975fc89967cfbe79500
prianthon <prianthonsubardio@gmail.com>
2016-12-23 06:13:21 +0700

    migrate reg admin

--- sidproject/pengaturan_umum/models.py
+++ sidproject/pengaturan_umum/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-class Pengaturan(models.Model):
+@python_2_unicode_compatible # mendukung Python 2
+class PengaturanUmum(models.Model):
+    nama_desa = models.CharField(max_length=100)
+    kode_desa = models.CharField(max_length=100)
+    nama_kepala_desa = models.CharField(max_length=100)
+    nip_kepala_desa = models.CharField(max_length=100)
+    kodepos = models.CharField(max_length=6)
+    nama_kecamatan = models.CharField(max_length=100)
+    kode_kecamatan = models.CharField(max_length=100)
+    nama_kepala_camat = models.CharField(max_length=100)
+    nip_kepala_camat = models.CharField(max_length=100)
+    nama_kabupaten = models.CharField(max_length=100)
+    kode_kabupaten = models.CharField(max_length=100)
+    nama_provinsi = models.CharField(max_length=100)
+    kode_provinsi = models.CharField(max_length=100)
+    logo_desa = models.CharField(max_length=100)
+    logo_kabupaten = models.CharField(max_length=100)
+    alamat_kantor_desa = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nama_desa
